8278abe230aa65659f169c0253c6e409bbcce01b
mtrandev <milkotrandev@gmail.com>
2025-04-25 11:38:08 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 PORT=5555
-const mongoDBURL = 'mongodb+srv://milkotrandev:45Kluch8284@book-store.hm0rcfo.mongodb.net/?retryWrites=true&w=majority';
+const mongoDBURL = 'mongodb+srv://root:root@book-store.hm0rcfo.mongodb.net/?retryWrites=true&w=majority';
